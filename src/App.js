03928486b76036e1149e54253a4a76bf5725918c
chikezie22 <simonchikezie44@gmail.com>
2024-06-18 00:35:39 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,51 @@ import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./MyComponent"; // Import your new component
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
@@ -30,11 +75,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Pizza />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
         <MyComponent advice={advice} getAdvice={getAdvice} />
         <p>Advice count: {count}</p>
       </header>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/prosciutto.jpg" alt="prosciutto image" />
+      <h2>Pizza Prosciutto</h2>
+      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
     </div>
   );
 }
